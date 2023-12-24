51f72078fae79daf21db3842f05fbfc81d7dca37
calepayson <calepayson@mac.com>
2023-12-24 12:10:18 -0800

    Gets temp from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please input a temperature: ");
+
+    let mut temp = String::new();
+
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read line");
+
+    println!("You input: {temp}");
+
+    // let temp: u32 = match temp.trim().parse() {
+    //     Ok(num) => num,
+    //     Err(_) => continue,
+    // };
+
+    // println!("You")
 }
